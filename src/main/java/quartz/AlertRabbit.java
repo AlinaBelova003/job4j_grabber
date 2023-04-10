7345b4f7deac4d899ed4bb28b7e2c95e225a4b76
alina <alinka_belova_2003@mail.ru>
2023-04-10 17:57:26 +0300

    1.1. Job c параметрами [#260360 #408397]

--- src/main/java/quartz/AlertRabbit.java
+++ src/main/java/quartz/AlertRabbit.java
@@ -3,34 +3,32 @@ package quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.ScheduledFuture;
 
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 /**
- * Quartz — очень сложный фреймворк. Он хранит выполненные работы в БД и для того, чтобы его настроить, необходимо создать множество бинов, один из которых главный – Quartz Sheduler.
- * После создания интерфейс IScheduler можно использовать для добавления, удаления и перечисления заданий и триггеров, а также для выполнения других операций, связанных с планированием (например, приостановка триггера).
- * Объект sсheduler - через него идет подключение. В этот объект добавляются все задачи, которые выполняются периодически
- * JobDetail — используется для определения экземпляров Jobs.
- * JobDataMap используется для хранения любого количества объектов данных, которые мы хотим сделать доступными для экземпляра задания при его выполнении.
- * Теперь вы можете спросить: «Как я могу предоставить свойства/конфигурацию для экземпляра Job?» и «Как я могу отслеживать состояние задания между выполнением?» Ответы на эти вопросы одинаковы: ключом является JobDataMap
- * Объект JobDetail создается клиентом Quartz во время добавления задания в планировщик. Позволяет описать детали выполнения()
- * При срабатывании триггера метод execute() вызывается и выводит его реализацию
+ * Quartz — фреймворк. Чтобы его настроить, необходимо создать множество бинов, один из которых главный – Sheduler.
+ * Его можно использовать для добавления, удаления и перечисления заданий и триггеров, других операций, связанных с планированием(приостановка триггера).
+ * JobDetail — используется для уточнения деталей работы
+ * JobDataMap используется для хранения любого количества объектов данных(свойств\ конфигураций), которые мы хотим сделать доступными для экземпляра задания при его выполнении.
  * Триггер – компонент, определяющий расписание, по которому будет выполняться данное Задание
- * Загрузка задачи и триггера в планировщик (scheduler.scheduleJob)
- * После выполнение работы в списке будут две даты
+ * При срабатывании триггера метод execute() вызывается и выводит его реализацию
+ * Загрузка задачи и триггера планировщику (scheduler.scheduleJob)
+ * scheduler.shutdown() - завершение\ закрытие планировщика
  */
-public class AlertRabbit {
+public class AlertRabbit implements AutoCloseable {
     public static void main(String[] args) {
-        try {
-            List<Long> store = new ArrayList<>();
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        try (Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler()) {
             scheduler.start();
             JobDataMap data = new JobDataMap();
-            data.put("store", store);
+            data.put("store", "?");
             JobDetail job = newJob(Rabbit.class)
                     .usingJobData(data)
                     .build();
@@ -42,12 +40,27 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-            Thread.sleep(5000);
+            Thread.sleep(10000);
             scheduler.shutdown();
-            System.out.println(store);
         } catch (SchedulerException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+
+    public static Properties readeProperties() {
+        Properties config = new Properties();
+        try (InputStream in = AlertRabbit2.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return config;
+    }
+
+    @Override
+    public void close() throws Exception {
+        throw new NullPointerException("Thrown from close()");
     }
 
     public static class Rabbit implements Job {
@@ -57,11 +70,13 @@ public class AlertRabbit {
         }
 
         /**
+         * Этот метод будет срабатывать каждый раз, когда будет срабатывать триггер
+         * Здесь помещен основной код
          * Чтобы получить объекты из context используется следующий вызов.
          */
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here ... ");
+            System.out.println("Запись выполнена в : ");
             List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
             store.add(System.currentTimeMillis());
         }
