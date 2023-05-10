ee32883560294526b9f0813d1b68606d75035c9e
alina <alinka_belova_2003@mail.ru>
2023-05-10 18:10:31 +0300

    4. Схема таблицы Post [#1731 #414427]

--- sql/post_alter.sql
+++ sql/post_alter.sql
@@ -1,7 +1,10 @@
-ALTER TABLE post DROP CONSTRAINT created;
+ALTER TABLE post DROP CONSTRAINT unique_created_id;
+ALTER TABLE post DROP CONSTRAINT post_created_key;
 
-CREATE UNIQUE INDEX CONCURRENTLY post_created_id ON post (created);
+CREATE UNIQUE INDEX CONCURRENTLY post_link_id ON post (link);
 
 ALTER TABLE post 
-ADD CONSTRAINT unique_created_id 
-UNIQUE USING INDEX post_created_id;
+ADD CONSTRAINT unique_link_id 
+UNIQUE USING INDEX post_link_id;
+
+
